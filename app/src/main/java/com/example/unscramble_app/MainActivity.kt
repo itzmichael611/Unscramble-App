33d903699be85bf6c05f1d82156c43f6c800cbbd
Michael <michaelbarnache6@gmail.com>
2026-08-17 21:52:44 +0800

    Phase 1

--- app/src/main/java/com/example/unscramble_app/MainActivity.kt
+++ app/src/main/java/com/example/unscramble_app/MainActivity.kt
@@ -3,45 +3,68 @@ package com.example.unscramble_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.unscramble_app.ui.theme.UnscrambleAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             UnscrambleAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GameScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun GameScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "UNSCRAMBLE",
+            fontSize = 30.sp
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnscrambleAppTheme {
-        Greeting("Android")
+        Text(
+            text = "TAC",
+            fontSize = 40.sp
+        )
+
+        Text(
+            text = "Unscramble the word!"
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = {
+                Text("Enter your answer")
+            }
+        )
+
+        Button(
+            onClick = { }
+        ) {
+            Text("SUBMIT")
+        }
+
+        Text(
+            text = "Score: 0"
+        )
     }
 }
